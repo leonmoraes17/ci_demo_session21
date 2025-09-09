520b2656630ef2b45eec05db5223e37d2303b674
leonmoraes17 <leonm4787@gmail.com>
2025-09-08 21:12:14 -0400

    changes to code

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
-def sub(a,b):
-    return a-b
+def sub(a, b):
+    return a - b
 
-result1 = add(2,3)
+result1 = add(2, 3)
 
-result2 = sub(5,1)
+result2 = sub(5, 1)
 
-print(result1, result1)
+print(result1, result2)
 
 # we are running this code to see if there are any linting errors
